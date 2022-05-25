afc49e4741ccf00904eff54051a3c8937aad1e78
Алексей <litos2@sibmail.com>
2022-05-26 02:07:36 +0700

    На проверку

---
+++ source/js/main.js
@@ -0,0 +1,23 @@
+
+
+const header = document.querySelector('header');
+header.classList.remove('main-header--no-js');
+
+const navButton = header.querySelector('.main-nav__toggle');
+let isOpened = false;
+
+const showMenu = () => {
+    isOpened = true;
+    header.classList.add('main-header--showed');
+}
+
+const closeMenu = () => {
+    isOpened = false;
+    header.classList.remove('main-header--showed');
+}
+
+navButton.addEventListener('click', () => {
+    isOpened
+        ? closeMenu()
+        : showMenu();
+})
